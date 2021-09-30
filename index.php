ee92b6f680754352a277aa64b11ad5d0b76e3d59
RafaelMendon <tafarelgames@gmail.com>
2021-09-30 22:51:49 +0000

    index

--- index.php
+++ index.php
@@ -17,7 +17,6 @@ $generos = $comando->fetchAll(PDO::FETCH_ASSOC);
 <head>
     <meta charset="UTF-8">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
     <title>BIBLIOTECA</title>
 </head>
 <body>
